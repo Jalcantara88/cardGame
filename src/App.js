a15384d69a71c0ca10e95b48a423fd0d811996f2
Julio Alcantara <jalcantara88@gmail.com>
2021-08-03 04:05:34 -0700

    set up background visuals and links

--- src/App.js
+++ src/App.js
@@ -28,8 +28,39 @@ function onPlayerCount(count, users) {
 function App() {
   return (
     <>
-      <div className="gameArea">
-        <Cards />
+      <div className="app">
+        <main className="gameArea">
+          <header>
+            <h1 className="gameTitle">
+              Remember Me
+            </h1>
+          </header>
+          <Cards />
+        </main>
+        <article className="aboutArea">
+          <header className="aboutHeader">About the Project</header>
+          <div className="aboutDetails">
+            <p>
+              This is a card game built in React using hooks. This is for a Hackathon hosted by Mintbean for Junior Devs. We had 1 week to make a card game using a standard deck. So I decided to make a calm memory game.
+            </p>
+            <p>
+              For this project I decided to not make it in PhaserJS since I wanted to challenge myself to see if I could make a game outside of a game engine. This was good practice in game logic and css tricks.
+            </p>
+            <p>
+              TODO: this project works currently as a single player game, but I have intention to finish the multiplayer aspect of this very soon.
+            </p>
+          </div>
+        </article>
+        <footer>
+          <h3>find me here:</h3>
+          <ul className="socialLinks">
+          <li><a href="https://github.com/Jalcantara88"><i class="socialLink fab fa-github fa-2x"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/deadheadstudio/"><i class="socialLink fab fa-linkedin fa-2x"></i></a></li>
+                            <li><a href="https://jalcantara.netlify.app/"><i class="socialLink fas fa-folder fa-2x"></i></a></li>
+                            <li><a href="https://www.youtube.com/channel/UCzLJQTYgUrhEFAOxt2ek0lg?"><i class="socialLink fab fa-youtube fa-2x"></i></a></li>
+                            <li><a href="https://www.behance.net/DeadHead"><i class="socialLink fab fa-behance-square fa-2x"></i></a></li>
+          </ul>
+        </footer>
       </div>
     </>
     
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,36 +1,101 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import shuffleDeck from './shuffle';
 
-var suitCount = 2;
-var cardIndex = 0;
+var suitCount = 0;
+var cardCount = 0;
+var gameOver = false;
 
-function handleClick(canSelect,event) {
-    if(canSelect) {
+function showCards(nodeList) { 
+    nodeList.forEach(card => {
+        card.classList.toggle("flipped");
+        setTimeout(() => {
+            card.classList.toggle("flipped");
+        }, 1000);
+    });
+}
+
+function handleClick(event, setNodeSelected) {
         const flipCardInner = event.target.parentNode;
+        console.log(flipCardInner);
         flipCardInner.classList.toggle("flipped");
-        console.log(event.target.ownerDocument);
+        setNodeSelected(node => [
+            ...node,
+            flipCardInner
+        ]); 
+}
+
+function addOrRemove(array, value, setArray, setNodes) {
+    var index = array.indexOf(value);
+
+    if(index === -1) {
+        setArray(old => [...old, value]);
+        //console.log(array);
+    }
+    else {
+        setArray(array.filter(item => item !== value));
+        setNodes([]);
+        //console.log(array);
     }
 }
 
-const RenderCard = ({card, canSelect}) => {
-    console.log("rendering card");
-    return(
-        <>
-            <div className="flip-card">
-            <div className="flip-card-inner" onClick={(e) => {
-                handleClick(canSelect, e);
-                }}>
-                <div className="flip-card-front">
-                    
-                </div>
-                <div className="flip-card-back">
-                    <h1 className="cardFace" style={{color: card.color}}>{card.face}</h1>
-                <img className="cardImg" style={{fillColor: card.color}} src={card.suitUrl} alt={card.suit}/>
+function checkPair(pair, setPair, nodePair, setNodePair) {
+    if(pair[0].value === pair[1].value) {
+        console.log("found a pair");
+        setTimeout(() => {
+            removePair(setPair, nodePair, setNodePair);
+          }, 1000);
+    }
+    else {
+        console.log("not a pair");
+        setTimeout(() => {
+            resetPair(setPair, nodePair, setNodePair);
+        }, 1000);
+    }
+}
 
+function resetPair(setPair, nodePair, setNodePair) {
+    console.log(nodePair);
+    setPair([]);
+    for(let i = 0; i < 2; i++) {
+        nodePair[i].classList.toggle("flipped");
+    }
+    setNodePair([]);
+}
+
+function removePair(setPair, nodePair, setNodePair) {
+    console.log(nodePair);
+    setPair([]);
+    for(let i = 0; i < 2; i++) {
+        nodePair[i].style.opacity = 0;
+    }
+    setNodePair([]);
+    cardCount -= 2;
+}
+
+
+const RenderCard = ({card, canSelect, setSelected, selected, setNodeSelected, id, setNodeDeck}) => {
+    //console.log("rendering card"); 
+    return(
+            <div className="flip-card">
+                <div className="flip-card-inner" onClick={(e) => {
+                    if(canSelect && selected.length < 2 && !gameOver) {
+                        handleClick(e, setNodeSelected);
+                        //setSelected(old => [...old, card]);
+                        addOrRemove(selected, card, setSelected, setNodeSelected);
+                        
+                        //setNodeSelected(old => [...old, e.target]);
+                    }
+                }} id={id}>
+                    <div className="flip-card-front">
+                        
+                    </div>
+                    <div className="flip-card-back">
+                        <h1 className="cardFace noClick" style={{color: card.color}}>{card.face}</h1>
+                    <img className="cardImg noClick" style={{fillColor: card.color}} src={card.suitUrl} alt={card.suit}/>
+
+                    </div>
                 </div>
-            </div>
             </div> 
-        </>
     )
 }
 
@@ -92,55 +157,111 @@ const makeCards = (setDeck, setDeckBuilt) => {
                 ...deck,
                 newCard
             ]);
-            cardIndex++;
         }
     }
     setDeckBuilt(true);
+    cardCount = suitCount * 13;
 }
 
+function checkWin() {
+    if(cardCount === 2) {
+        console.log("you win");
+        gameOver = true;
+    }
+    else {
+        console.log("keep playing");
+    }
+}
 const Cards = () => {
     const [deck, setDeck] = useState([]);
     const [deckBuilt, setDeckBuilt] = useState(false);
-    //const [selected, setSelected] = useState([]);
+    const [selected, setSelected] = useState([]);
     const [canSelect, setCanSelect] = useState(false);
+    const [nodeSelected, setNodeSelected] = useState([]);
+    const [nodeDeck, setNodeDeck] = useState([]);
+    const [gameStarted, setGameStarted] = useState(false);
+
+
+    useEffect(() => {
+        console.log(selected);
+        if(selected.length === 2) {
+            checkPair(selected, setSelected, nodeSelected, setNodeSelected);
+            checkWin();
+        }
+    },[selected]);
 
     if(!deckBuilt) {
         makeCards(setDeck, setDeckBuilt);
-        console.log(deck);
     }
     
 
     const renderedCards = deck.map((card, i) => {
-        console.log("rendering card " + i);
+        //console.log("rendering card " + i);
         return(
             <RenderCard
                 key={i}
                 card={card}
                 canSelect={canSelect}
-                 
+                setSelected={setSelected}
+                selected={selected}
+                setNodeSelected={setNodeSelected}
+                setNodeDeck={setNodeDeck}
+                id={i}
             />
         );
     })
 
-    return(
-        <div className="cardArea">
-            <button onClick={() => {
-                console.log("shuffled");
-                const newDeck = deck;
-                shuffleDeck(newDeck);
-                setDeck([...newDeck]);
-                console.log(deck);
-            }}>shuffle</button>
-            <button onClick={() => {
-                console.log("toggled Move");
-                console.log(canSelect);
-                setCanSelect(!canSelect);
-            }}>toggleMove</button>
-            <div className="cardHolder">
-                {renderedCards}
+    if(!gameStarted) {
+        return(
+            <section className="menu">
+                <h3 className="menuTitle">
+                    Please Select A Difficulty:
+                </h3>
+                <button className="menuButton" onClick={() => {
+                    suitCount = 2;
+                    makeCards(setDeck, setDeckBuilt);
+                    setGameStarted(true);
+                }}>Normal</button>
+                <button className="menuButton" onClick={() => {
+                    suitCount = 4;
+                    makeCards(setDeck, setDeckBuilt);
+                    setGameStarted(true);
+                }}>Hard</button>
+                <article className="instruct">
+                    <header className="instructTitle">Instructions</header>
+                    <p className="instructDetails">
+                        Welcome to Remember Me, a standard card based memory game. Select Difficulty of either normal(2 suits) or hard(4 suits). you only need your mouse to click on pairs of cards until you have matched them all. There is no rush, take your time. Relax. and enjoy yourself.
+                    </p>
+                    <p> More Features to come!</p>
+                </article>
+            </section>
+        )
+    }
+
+    else {
+        return(
+            <div className="cardArea">
+                <button onClick={() => {
+                    console.log("shuffled");
+                    const newDeck = deck;
+                    shuffleDeck(newDeck);
+                    setDeck([...newDeck]);
+                    console.log(deck);
+                }}>shuffle</button>
+                <button onClick={() => {
+                    console.log("toggled Move");
+                    console.log(canSelect);
+                    setCanSelect(!canSelect);
+                }}>toggleMove</button>
+                <button onClick={() => {
+                    showCards(nodeDeck);
+                }}>showCards</button>
+                <div className="cardHolder">
+                    {renderedCards}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Cards;
